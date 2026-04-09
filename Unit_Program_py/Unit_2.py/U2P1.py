6fe7e6c4941e4b5766ba783541969986f759b7e2
Sipai-Asif <mohamadasif.sipai131235@marwadiuniversity.ac.in>
2026-04-09 08:42:13 +0530

    Delete Unit_Program_py directory

--- Unit_Program_py/Unit_2.py/U2P1.py
+++
@@ -1,9 +0,0 @@
-#WAP to input a number and print whether it is even or odd number.
-
-a=int(input("Enter number: "))
-
-if(a%2==0):
-    print("Number is even")
-
-else:
-    print("Number is odd")
--- Unit_Program_py/Unit_4.py/U4P10.py
+++
@@ -1,6 +0,0 @@
-'''Demo for import module in python  '''
-
-import mymodule
-
-mymodule.greet("Asif")
-mymodule.add(10, 20)
